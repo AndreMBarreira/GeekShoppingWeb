2279954bbb631065fb9d61f3e8a0cd0dc372d547
AndreMBarreira <andremilla.am@gmail.com>
2024-10-11 10:55:32 -0300

    Connection String Configuration working

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GeekShopping.ProductAPI.Models.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MySQLContext") ?? throw new InvalidOperationException("Connection string 'MySQLContext' not found.")));
+
 
 // Add services to the container.
 
