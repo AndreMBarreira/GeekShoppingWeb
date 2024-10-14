613e75f1d4c6952999e4c3737d44cb61aa4f482f
AndreMBarreira <andremilla.am@gmail.com>
2024-10-14 10:24:00 -0300

    IProductRepository

--- GeekShopping/GeekShopping.ProductAPI/Data/DTO/ProductDTO.cs
+++ GeekShopping/GeekShopping.ProductAPI/Data/DTO/ProductDTO.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace GeekShopping.ProductAPI.Data.DTO
+﻿namespace GeekShopping.ProductAPI.Data.DTO
 {
     public class ProductDTO
     {
