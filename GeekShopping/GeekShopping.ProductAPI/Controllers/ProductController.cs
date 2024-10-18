e7e9681833db395c65fda3270ad268d2884ad3a3
AndreMBarreira <andremilla.am@gmail.com>
2024-10-18 10:36:42 -0300

    Fixing Annotation FromBody on ProductController API

--- GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -33,7 +33,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductDTO>> Create(ProductDTO dto)
+        public async Task<ActionResult<ProductDTO>> Create([FromBody] ProductDTO dto)
         {
             if (dto == null) return BadRequest();
             var productDTO = await _repository.Create(dto);
@@ -41,7 +41,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductDTO>> Update(ProductDTO dto)
+        public async Task<ActionResult<ProductDTO>> Update([FromBody] ProductDTO dto)
         {
             if (dto == null) return BadRequest();
             var productDTO = await _repository.Update(dto);
